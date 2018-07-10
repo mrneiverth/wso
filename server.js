c92bffea1ca5c51633a46a4fe6affe6d1fa6fcd7
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2018-07-10 16:19:35 -0300

    Conseguindo pegar dados meio porcamente.

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 var dps = require('dbpedia-sparql-client').default;// npm install dbpedia-sparql-client
-var query = 'SELECT ?animal str(?nome) as ?Nome str(?ordem_label) as ?Ordem ?status ?data_extincao WHERE { ?animal dbo:class dbr:Mammal ; foaf:name ?nome ; dbo:order ?ordem . ?animal dbo:conservationStatus ?status . FILTER ((?ordem = dbr:Rodent) || (?ordem = dbr:Carnivora)) ?ordem rdfs:label ?ordem_label FILTER ( langMatches(lang(?ordem_label),"pt") ) FILTER ( (?status = "EX") || (?status = "EW") || (?status = "PE") || (?status = "PEW") || (?status = "CR") || (?status = "EN") || (?status = "VU") ) #EX - extinct #EW - extinct in the wild #CR - critically endangered #EN - endangered #VU - vulnerable OPTIONAL { ?animal dbp:extinct ?data_extincao . } } ORDER BY ?status';
+var query = 'SELECT ?animal str(?nome) as ?Nome str(?ordem_label) as ?Ordem ?status ?data_extincao WHERE { ?animal dbo:class dbr:Mammal ; foaf:name ?nome ; dbo:order ?ordem . ?animal dbo:conservationStatus ?status . FILTER ((?ordem = dbr:Rodent) || (?ordem = dbr:Carnivora)) ?ordem rdfs:label ?ordem_label FILTER ( langMatches(lang(?ordem_label),"pt") ) FILTER ( (?status = "EX") || (?status = "EW") || (?status = "PE") || (?status = "PEW") || (?status = "CR") || (?status = "EN") || (?status = "VU") ) OPTIONAL { ?animal dbp:extinct ?data_extincao . } } ORDER BY ?status';
+// var query = 'SELECT DISTINCT ?Concept WHERE {[] a ?Concept} LIMIT 10';
 
-var mydata = dps.client()
-  .query(query)
-  .timeout(15000) // optional, defaults to 10000
-  .asJson() // or asXml()
-  .then(function(r) { /* handle success */ })
-  .catch(function(e) { /* handle error */ });
 
 var http = require('http');
 
@@ -20,3 +15,20 @@ var server = http.createServer(function(req, res) {
 });
 
 server.listen(8080);
+
+
+var mydata = dps.client()
+  .query(query)
+  .timeout(15000) // optional, defaults to 10000
+  .asJson() // or asXml()
+  .then(function(r) {
+    /* handle success */
+    console.log("Sucesso");
+    console.log(r["results"]["bindings"]);
+    console.log(r["results"]["bindings"].length + " entradas encontradas.");
+  })
+  .catch(function(e) {
+    /* handle error */
+    console.log("Erro");
+    console.log(e);
+  });
