bf9bb1a70df99681188d31afb3ce6eecd8e3d77f
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2018-07-16 21:45:41 -0300

    merge

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const favicon = require('express-favicon');
 const app = express()
 
 const dps = require('dbpedia-sparql-client').default;// npm install dbpedia-sparql-client
-const query = 'select distinct * where { ?animal dbo:kingdom dbr:Animal; dbo:phylum ?filo ; dbo:class ?classe; foaf:name ?nome ; rdfs:label ?label; dbo:abstract ?abstract; rdfs:comment ?comment; foaf:isPrimaryTopicOf ?link_wikipedia; dbo:thumbnail ?thumbnail filter(lang(?label) = "pt" && lang(?abstract) = "pt" && lang(?comment) = "pt") filter( ?classe = dbr:Chondrichthyes || ?classe = dbr:Actinopterygii || ?classe = dbr:Osteichthyes || ?classe = dbr:Amphibia || ?classe = dbr:Sauropsida || ?classe = dbr:Reptilia || ?classe = dbr:Aves || ?classe = dbr:Mammalia || ?filo = dbr:Arthropod ) } order by ?classe';
+const query = 'select distinct * where { ?animal dbo:kingdom dbr:Animal; dbo:phylum ?filo ; dbo:class ?classe; foaf:name ?nome ; rdfs:label ?label; dbo:abstract ?abstract; rdfs:comment ?comment; foaf:isPrimaryTopicOf ?link_wikipedia; dbo:thumbnail ?thumbnail filter(lang(?label) = "pt" && lang(?abstract) = "pt" && lang(?comment) = "pt") filter( ?classe = dbr:Chondrichthyes || ?classe = dbr:Actinopterygii || ?classe = dbr:Osteichthyes || ?classe = dbr:Amphibia || ?classe = dbr:Sauropsida || ?classe = dbr:Reptilia || ?classe = dbr:Aves || ?classe = dbr:Mammalia  ) } order by ?classe';
 var tools = require("./tools.js");
 
 app.use(favicon('favicon.png'));
@@ -38,10 +38,21 @@ app.get('/busca', (request, response) => {
       // console.log("ReadRankFromDb: " + ten_ranked_rows[1].nome + "\n" + ten_ranked_rows[1].pontos);
       // tools.InsertSingleRowInDb("matheus", 500);
       //[/db stuffs]
-      
+      var size = r["results"]["bindings"].length;
+      var set = new Set();
+      for(i = 0; i < 3; i++){
+        var num = Math.floor((Math.random() * size));
+        set.add(num);
+        while(set.has(num)) {
+          num = Math.floor((Math.random() * size));
+        }
+      }
+      escolhidos = [...set];
+
+
       response.render('home', {
         busca: 'var query = '+ JSON.stringify(r) + ';',
-        alo: r["results"]["bindings"].slice(0,10)
+        escolhidos: escolhidos
       })
     })
     .catch(function(e) {
